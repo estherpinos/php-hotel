b41faf04f5f1699df926db672724841ddaec97b0
estherpinos <estherpinos1@gmail.com>
2023-10-26 15:27:21 +0200

    Add [table con dati]

--- index.php
+++ index.php
@@ -40,4 +40,69 @@
 
     ];
 
+    $parking=$_GET['parking'];
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php hotel</title>
+
+  <!-- BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+
+  <div class="container mt-5">
+
+    <form action="/php-hotel" method="GET">
+      <div class="mb-3 form-check">
+        <input type="checkbox" name="parking" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Parking</label>
+      </div>
+     
+      <button type="submit" class="btn btn-primary text-center ">Submit</button>
+    </form>
+
+    <table class="table mt-5 ">
+      <thead>
+        <tr>
+        
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Parking</th>
+          <th scope="col">Vote</th>
+          <th scope="col">Distance to center</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($hotels as $hotel) {  ?>
+
+          <?php 
+            if($parking){
+              if($hotel['parking']){ ?>
+                <tr class="">
+                  <td> <?php echo $hotel['name'] ?> </td>
+                  <td> <?php echo $hotel['description'] ?> </td>
+                  <td> <?php echo $hotel['parking'] ?> </td>
+                  <td> <?php echo $hotel['vote'] ?> </td>
+                  <td> <?php echo $hotel['distance_to_center'] ?> </td>  
+                </tr>
+              <?php
+              } ?>
+            <?php } ?>
+          
+        <?php } ?>
+      
+      </tbody>
+    </table>
+  </div>
+
+  
+</body>
+</html>
